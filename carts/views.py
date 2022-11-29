0e58586fbb568213a5020b6eebbda06a38d195b2
LeaderMaharshi <leadermaharshi@gmail.com>
2022-11-29 20:10:28 +0530

    Variation Manager Added

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from . models import Cart, CartItem
 from django.http import HttpResponse
 
@@ -12,11 +12,19 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product_variation = []
     if request.method == 'POST':
-        color = request.POST['color']
-        size = request.POST['size']
-
-    
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            
+            try:
+                variation = Variation.objects.get(variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                pass
+               
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id = _cart_id(request))
