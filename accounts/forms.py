b3e9159908a17d6098b5e8352d7b2ddcd1d80f2b
LeaderMaharshi <leadermaharshi@gmail.com>
2022-12-06 10:42:31 +0530

    __init__ modification for class and placeholders

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,23 @@
 # from django import forms
-from django.forms import ModelForm
+from django import forms
 from .models import Account
 
-class RegistrationForm(ModelForm):
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter your password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+    
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your Email'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
