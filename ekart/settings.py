adb23c671274e5880fab080a8761e573f2e612a7
LeaderMaharshi <leadermaharshi@gmail.com>
2022-12-12 18:55:36 +0530

    Email stmp added

--- ekart/settings.py
+++ ekart/settings.py
@@ -146,3 +146,15 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+#SMTP CONFIG
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'maharshirm546@gmail.com'
+EMAIL_HOST_PASSWORD = 'qnbavsclcwrjnfnp'
+EMIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEBUG = True
+
+#ifcqoilkcpfoxgtp
+#qnbavsclcwrjnfnp
