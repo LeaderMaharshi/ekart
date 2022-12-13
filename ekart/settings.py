30f9e97dfac6ce5deeaf692ef8f3f0d8a844246e
LeaderMaharshi <leadermaharshi@gmail.com>
2022-12-13 18:30:35 +0530

    activation email verification added

--- ekart/settings.py
+++ ekart/settings.py
@@ -152,7 +152,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'maharshirm546@gmail.com'
 EMAIL_HOST_PASSWORD = 'qnbavsclcwrjnfnp'
-EMIL_USE_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEBUG = True
 
