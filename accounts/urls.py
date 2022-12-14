739b191c32b4b7188fdf9bcacdc5266855f59902
LeaderMaharshi <leadermaharshi@gmail.com>
2022-12-14 19:38:42 +0530

    Reset password added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
@@ -10,4 +11,8 @@ urlpatterns = [
     
     
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgotpassword/', views.forgot_password, name='forgot_password'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
+    
 ]
